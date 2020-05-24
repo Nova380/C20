6fbfc019d6f42433dc406edcaf5ae08135d6897f
Nova380 <65493282+Nova380@users.noreply.github.com>
2020-05-24 17:41:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C20
-THIS IS
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(800,400);
+  fixrect=createSprite(400, 200, 50, 50);
+  movingrect=createSprite(400,200,50,50);
+}
+
+function draw() {
+  background(0,0,0);  
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if(fixrect.x-movingrect.x<=fixrect.width/2+movingrect.width/2&&
+    movingrect.x-fixrect.x<=fixrect.width/2+movingrect.width/2&&
+    movingrect.y-fixrect.y<=fixrect.height/2+movingrect.height/2&&
+    fixrect.y-movingrect.y<=fixrect.height/2+movingrect.height/2){
+    fixrect.shapeColor="violet";
+    movingrect.shapeColor="violet";
+  }
+  else{
+  fixrect.shapeColor="green";
+  movingrect.shapeColor="green"; 
+  }
+  drawSprites();
+}
